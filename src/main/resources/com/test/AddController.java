4cb4e0c617c977d14d945a9f59779187a581bd85
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-01-30 19:28:54 +0545

    repo init with mvc setup

---
+++ src/main/resources/com/test/AddController.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.lang.System;
+import org.springframework.core.SpringVersion;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddController {
+	
+	@RequestMapping("/add")
+	public void add()
+	{
+		string ver = SpringVersion.getVersion();
+		System.out.println("I am here");
+		System.out.println(ver);
+	}
+}
